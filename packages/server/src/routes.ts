6ddbd3da0fa34a3b93dec9442cebb66a8ae24656
Brenda <brendaprofiro@gmail.com>
2021-03-13 13:19:58 -0300

    chore: Create session route

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import UserRepository from './repositories/UserRepository';
 import CreateUserService from './services/CreateUserService';
+import AuthenticateUserService from './services/AuthenticateUserService';
 
 const routes = express.Router();
 
@@ -21,6 +22,20 @@ routes.post('/users', async (request, response) => {
     });
 
     return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+routes.post('/session', async (request, response) => {
+  const { email, password } = request.body;
+
+  try {
+    const authenticateUserService = new AuthenticateUserService(userRepository);
+
+    const session = await authenticateUserService.execute({ email, password });
+
+    return response.json(session);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
