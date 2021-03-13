9f56c5be300111a25ca86f6648176766ec2c144b
Brenda <brendaprofiro@gmail.com>
2021-03-13 10:48:05 -0300

    chore: Create Input component

---
+++ packages/app/src/components/Input/index.tsx
@@ -0,0 +1,95 @@
+import { useField } from '@unform/core';
+import React, {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, Icon, TextInput } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon?: string;
+  containerStyles?: {};
+}
+
+interface InputValueReference {
+  value: string;
+}
+
+interface InputRef {
+  focus(): void;
+}
+
+const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
+  { name, icon, containerStyles = {}, ...rest },
+  ref,
+) => {
+  const inputElementRef = useRef<any>(null);
+
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFielled, setIsFielled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFielled(!!inputValueRef.current.value);
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container
+      style={containerStyles}
+      isFocused={isFocused}
+      isErrored={!!error}
+    >
+      {icon && (
+        <Icon
+          name={icon}
+          size={20}
+          color={isFocused || isFielled ? '#FF872C' : '#969CB2'}
+        />
+      )}
+
+      <TextInput
+        ref={inputElementRef}
+        keyboardAppearance="dark"
+        placeholderTextColor="#969CB2"
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        onChangeText={(value) => {
+          inputValueRef.current.value = value;
+        }}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default forwardRef(Input);
---
+++ packages/app/src/components/Input/styles.ts
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components';
+import { View, TextInput as InputElement } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled(View)<ContainerProps>`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  background: #fff;
+  border-radius: 10px;
+  margin-bottom: 16px;
+  border: 2px;
+  border-color: #fff;
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+  ${(props) =>
+    props.isFocused &&
+    css`
+      border-color: #ff872c;
+    `}
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TextInput = styled(InputElement)`
+  flex: 1;
+  color: #969cb2;
+  font-size: 16px;
+  font-family: 'Poppins_400Regular';
+
+  /* font-family: 'RoboSlab-Regular'; */
+`;
+
+export const Icon = styled(FeatherIcon)`
+  margin-right: 16px;
+`;
