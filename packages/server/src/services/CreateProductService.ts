fcc2dae50e227eaf4b773588a85ee8191d0f81fb
Brenda <brendaprofiro@gmail.com>
2021-03-14 21:59:09 -0300

    chore: Create service to add new products

---
+++ packages/server/src/services/CreateProductService.ts
@@ -0,0 +1,49 @@
+import Product from '../models/Product';
+import ProductRepository from '../repositories/ProductRepository';
+import TransactionRepository from '../repositories/TransactionRepository';
+
+class CreateProduct {
+  private productRepository: ProductRepository;
+
+  private transactionRepository: TransactionRepository;
+
+  constructor(transactionRepository: ProductRepository) {
+    this.productRepository = transactionRepository;
+  }
+
+  public async execute({
+    name,
+    price,
+    code,
+    quantity,
+    user_id: userId,
+  }: Omit<Product, 'id'>): Promise<Product> {
+    const checkExistingProduct = await this.productRepository.findByCode(code);
+
+    await this.transactionRepository.create({
+      title: name,
+      type: 'outcome',
+      value: (price * quantity) * -1,
+      user_id: userId,
+      date: new Date(),
+    });
+
+    if (checkExistingProduct) {
+      checkExistingProduct.quantity += quantity;
+
+      return checkExistingProduct;
+    }
+
+    const product = await this.productRepository.create({
+      name,
+      code,
+      price,
+      quantity,
+      user_id: userId,
+    });
+
+    return product;
+  }
+}
+
+export default CreateProduct;
