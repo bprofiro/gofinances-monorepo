50014114dc49b26994ce53576504ec51aee7f1f0
Brenda <brendaprofiro@gmail.com>
2021-03-14 21:58:02 -0300

    chore: Create Products repository

---
+++ packages/server/src/repositories/ProductRepository.ts
@@ -0,0 +1,47 @@
+import Product from '../models/Product';
+
+class ProductRepository {
+  private products: Product[];
+
+  constructor() {
+    this.products = [];
+  }
+
+  public create({
+    name,
+    price,
+    code,
+    quantity,
+    user_id: userId,
+  }: Omit<Product, 'id'>): Product {
+    const product = new Product({
+      name,
+      price,
+      code,
+      quantity,
+      user_id: userId,
+    });
+
+    this.products.push(product);
+
+    return product;
+  }
+
+  public async findByUser(userId: string): Promise<Product[]> {
+    const products = this.products.filter(
+      (productItem) => productItem.user_id === userId && productItem.quantity > 0,
+    );
+
+    return products;
+  }
+
+  public async findByCode(code: string): Promise<Product | undefined> {
+    const product = this.products.find(
+      (productItem) => productItem.code === code,
+    );
+
+    return product;
+  }
+}
+
+export default ProductRepository;
